bf11f72e19bb78ac87718983e156276ca52f7916
Ilya Drygin <beroldset@gmail.com>
2020-02-10 18:48:14 +0300

    front changes

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Chat from './Chat'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <Chat />
+    </div>
+  );
+}
+
+export default App;
